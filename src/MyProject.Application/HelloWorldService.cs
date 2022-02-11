0f52846d3bb4b3a949bdfa19b6a031516972b0e6
Jakeuj <jakeuj@hotmail.com>
2022-02-11 16:49:17 +0800

    inject IAbpLazyServiceProvider

--- src/MyProject.Application/HelloWorldService.cs
+++ src/MyProject.Application/HelloWorldService.cs
@@ -1,10 +1,15 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Volo.Abp.DependencyInjection;
 
 namespace MyProject.Application;
 
 public class HelloWorldService : MyProjectAppService
 {
+    public HelloWorldService(IAbpLazyServiceProvider lazyServiceProvider)
+    {
+        LazyServiceProvider = lazyServiceProvider;
+    }
     public Task SayHelloAsync()
     {
         Logger.LogInformation("Hello World!");
