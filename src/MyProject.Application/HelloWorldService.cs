5ac77dfe9d6ba5c3a8c67e197a35e7985f031d32
Jakeuj <jakeuj@hotmail.com>
2022-02-11 16:37:36 +0800

    Add console service

---
+++ src/MyProject.Application/HelloWorldService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace MyProject.Application;
+
+public class HelloWorldService : MyProjectAppService
+{
+    public Task SayHelloAsync()
+    {
+        Logger.LogInformation("Hello World!");
+        return Task.CompletedTask;
+    }
+}
